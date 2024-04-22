e9c2e6a1d50d13d44574f4e4dad79a49b54f57b3
Carlos-David-Rojas-Coronado <crojasc3@miumg.edu.gt>
2024-04-22 09:52:36 -0600

    se creo la conexion y la base de datos

--- scripts/Prototipo_Segundo_parcial.sql
+++ scripts/Prototipo_Segundo_parcial.sql
@@ -1,6 +1,6 @@
-DROP DATABASE siu;
-CREATE DATABASE siu;
-USE siu;
+DROP DATABASE dap;
+CREATE DATABASE dap;
+USE dap;
 CREATE TABLE IF NOT EXISTS usuario (
   id_usuario INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(60) NOT NULL,
@@ -8,142 +8,14 @@ CREATE TABLE IF NOT EXISTS usuario (
   PRIMARY KEY (id_usuario) )
 ENGINE = InnoDB CHARACTER SET = latin1;
 -- -----------------------------------------------------
--- Table `educativo`.`Alumnos`
+-- Table `educativo`.`habitacion`
 -- -----------------------------------------------------
-CREATE TABLE alumnos
+CREATE TABLE habitacion
  (
-  carnet_alumno VARCHAR(15),
-  nombre_alumno VARCHAR(45),
-  direccion_alumno VARCHAR(45),
-  telefono_alumno VARCHAR(45),
-  email_alumno VARCHAR(20),
-  estatus_alumno VARCHAR(1),
-  PRIMARY KEY (carnet_alumno)
+  id_habitacion VARCHAR(15),
+  id_tipoHabitacion VARCHAR(45),
+  id_tipocama VARCHAR(45),
+  precio VARCHAR(20),
+  estatus_habitacion VARCHAR(1),
+  PRIMARY KEY (id_habitacion)
 ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
-
--- -----------------------------------------------------
--- Table `educativo`.`Maestros`
--- -----------------------------------------------------
-CREATE TABLE maestros
-(
-  codigo_maestro VARCHAR(5),
-  nombre_maestro VARCHAR(45),
-  direccion_maestro VARCHAR(45),
-  telefono_maetro VARCHAR(45),
-  email_maestro VARCHAR(20),
-  estatus_maestro VARCHAR(1),
-  PRIMARY KEY (codigo_maestro)
- ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Facultades`
--- -----------------------------------------------------
-CREATE TABLE facultades
-(
-  codigo_facultad VARCHAR(5),
-  nombre_facultad VARCHAR(45),
-  estatus_facultad VARCHAR(1),
-  PRIMARY KEY (codigo_facultad)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Carreras`
--- -----------------------------------------------------
-CREATE TABLE carreras
-(
-  codigo_carrera VARCHAR(5),
-  nombre_carrera VARCHAR(45),
-  codigo_facultad VARCHAR(5),
-  estatus_carrera VARCHAR(1),
-  PRIMARY KEY (codigo_carrera),
-  FOREIGN KEY (codigo_facultad) REFERENCES facultades(codigo_facultad)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Cursos`
--- -----------------------------------------------------
-CREATE TABLE cursos
-(
-  codigo_curso VARCHAR(5),
-  nombre_curso VARCHAR(45),
-  estatus_curso VARCHAR(1),
-  PRIMARY KEY (codigo_curso)
- ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Secciones`
--- -----------------------------------------------------
-CREATE TABLE secciones
-(
-  codigo_seccion VARCHAR(5),
-  nombre_seccion VARCHAR(45),
-  estatus_seccion VARCHAR(1),
-  PRIMARY KEY (codigo_seccion)
- ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Sedes`
--- -----------------------------------------------------
-CREATE TABLE sedes
-(
-  codigo_sede VARCHAR(5),
-  nombre_sede VARCHAR(45),
-  estatus_sede VARCHAR(1),
-  PRIMARY KEY (codigo_sede)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Aulas`
--- -----------------------------------------------------
-CREATE TABLE aulas
-(
-  codigo_aula VARCHAR(5),
-  nombre_aula VARCHAR(45),
-  estatus_aula VARCHAR(1),
-  PRIMARY KEY (codigo_aula)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE jornadas
-(
-	codigo_jornada VARCHAR(5),
-    nombre_jornada VARCHAR(45),
-    estatus_jornada VARCHAR(1),
-    PRIMARY KEY (codigo_jornada)
-) ENGINE=INNODB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Asignacion_cursos_alumnos`
--- -----------------------------------------------------
-CREATE TABLE asignacioncursosalumnos
-(
-  codigo_carrera VARCHAR(5),
-  codigo_sede VARCHAR(5),
-  codigo_jornada VARCHAR(5),
-  codigo_seccion VARCHAR(5),
-  codigo_aula VARCHAR(5),
-  codigo_curso VARCHAR(5),
-  carnet_alumno VARCHAR(15),
-  nota_asignacioncursoalumnos FLOAT(10,2), 
-  PRIMARY KEY (codigo_carrera, codigo_sede, codigo_jornada, codigo_seccion, codigo_aula, codigo_curso, carnet_alumno),
-  FOREIGN KEY (codigo_carrera) REFERENCES carreras(codigo_carrera),
-  FOREIGN KEY (codigo_sede) REFERENCES sedes(codigo_sede),
-  FOREIGN KEY (codigo_jornada) REFERENCES jornadas(codigo_jornada),
-  FOREIGN KEY (codigo_seccion) REFERENCES secciones(codigo_seccion),
-  FOREIGN KEY (codigo_aula) REFERENCES aulas(codigo_aula),
-  FOREIGN KEY (codigo_curso) REFERENCES cursos(codigo_curso),
-  FOREIGN KEY (carnet_alumno) REFERENCES alumnos(carnet_alumno)
-  ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- -----------------------------------------------------
--- Table `educativo`.`Asignacion_cursos_maestros`
--- -----------------------------------------------------
-CREATE TABLE asignacioncursosmastros
-(
-  codigo_carrera VARCHAR(5),
-  codigo_sede VARCHAR(5),
-  codigo_jornada VARCHAR(5),
-  codigo_seccion VARCHAR(5),
-  codigo_aula VARCHAR(5),
-  codigo_curso VARCHAR(5),
-  codigo_maestro VARCHAR(5),
-  PRIMARY KEY (codigo_carrera, codigo_sede, codigo_jornada, codigo_seccion, codigo_aula, codigo_curso),
-  FOREIGN KEY (codigo_carrera) REFERENCES carreras(codigo_carrera),
-  FOREIGN KEY (codigo_sede) REFERENCES sedes(codigo_sede),
-  FOREIGN KEY (codigo_jornada) REFERENCES jornadas(codigo_jornada),
-  FOREIGN KEY (codigo_seccion) REFERENCES secciones(codigo_seccion),
-  FOREIGN KEY (codigo_aula) REFERENCES aulas(codigo_aula),
-  FOREIGN KEY (codigo_curso) REFERENCES cursos(codigo_curso),
-  FOREIGN KEY (codigo_maestro) REFERENCES maestros(codigo_maestro)
-  ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
-
